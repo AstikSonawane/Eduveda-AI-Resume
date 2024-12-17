f3d4a31ace22f4508eb64f1815c5ccd29d7904c7
LAPTOP-C3OEI493 <astiksonawane12345@gmail.com>
2024-12-17 10:43:21 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -46,6 +46,14 @@ st.set_page_config(
 # Custom CSS for Modern UI
 st.markdown("""
     <style>
+        [data-testid="stAppViewContainer"] {
+        background-image: url('https://images.unsplash.com/photo-1642355008521-236f1d29d0a8?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+        background-size: cover;
+        background-position: center;
+        height: 100%;
+        padding-top: 50px; /* Adds some space for the header */
+    }
+
     /* App background color */
     .app-background {
         background-color: #EEEEEE;
